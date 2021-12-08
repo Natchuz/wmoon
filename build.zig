f3186100c494f972cca30a956deb411aef56db2a
Natchuz <30700722+Natchuz@users.noreply.github.com>
2021-12-08 23:15:37 +0100

    Updated build.zig and CI accordingly

--- build.zig
+++ build.zig
@@ -6,11 +6,11 @@ const zbs = std.build;
 
 const ScanProtocolsStep = @import("deps/zig-wayland/build.zig").ScanProtocolsStep;
 
-/// While a river release is in development, this string should contain the version in development
+/// While a wmoon release is in development, this string should contain the version in development
 /// with the "-dev" suffix.
 /// When a release is tagged, the "-dev" suffix should be removed for the commit that gets tagged.
 /// Directly after the tagged commit, the version should be bumped and the "-dev" suffix added.
-const version = "0.2.0-dev";
+const version = "0.0.1-dev";
 
 pub fn build(b: *zbs.Builder) !void {
     const target = b.standardTargetOptions(.{});
@@ -19,39 +19,10 @@ pub fn build(b: *zbs.Builder) !void {
     const xwayland = b.option(
         bool,
         "xwayland",
-        "Set to true to enable xwayland support",
-    ) orelse false;
-
-    const man_pages = b.option(
-        bool,
-        "man-pages",
-        "Set to true to build man pages. Requires scdoc. Defaults to true if scdoc is found.",
-    ) orelse scdoc_found: {
-        _ = b.findProgram(&[_][]const u8{"scdoc"}, &[_][]const u8{}) catch |err| switch (err) {
-            error.FileNotFound => break :scdoc_found false,
-            else => return err,
-        };
-        break :scdoc_found true;
-    };
-
-    const bash_completion = b.option(
-        bool,
-        "bash-completion",
-        "Set to true to install bash completion for riverctl. Defaults to true.",
+        "Set to false to disable xwayland support",
     ) orelse true;
 
-    const zsh_completion = b.option(
-        bool,
-        "zsh-completion",
-        "Set to true to install zsh completion for riverctl. Defaults to true.",
-    ) orelse true;
-
-    const fish_completion = b.option(
-        bool,
-        "fish-completion",
-        "Set to true to install fish completion for riverctl. Defaults to true.",
-    ) orelse true;
-
+    // Obtain full version name
     const full_version = blk: {
         if (mem.endsWith(u8, version, "-dev")) {
             var ret: u8 = undefined;
@@ -70,6 +41,7 @@ pub fn build(b: *zbs.Builder) !void {
         }
     };
 
+    // Create scanner to generate code files based on Wayland protocols
     const scanner = ScanProtocolsStep.create(b);
     scanner.addSystemProtocol("stable/xdg-shell/xdg-shell.xml");
     scanner.addSystemProtocol("unstable/pointer-gestures/pointer-gestures-unstable-v1.xml");
@@ -81,101 +53,32 @@ pub fn build(b: *zbs.Builder) !void {
     scanner.addProtocolPath("protocol/wlr-layer-shell-unstable-v1.xml");
     scanner.addProtocolPath("protocol/wlr-output-power-management-unstable-v1.xml");
 
-    {
-        const river = b.addExecutable("river", "river/main.zig");
-        river.setTarget(target);
-        river.setBuildMode(mode);
-        river.addBuildOption(bool, "xwayland", xwayland);
-        river.addBuildOption([:0]const u8, "version", full_version);
+    // Main executable build
+    const wmoon_build = b.addExecutable("wmoon", "src/main.zig");
+    wmoon_build.setTarget(target);
+    wmoon_build.setBuildMode(mode);
+    wmoon_build.addBuildOption(bool, "xwayland", xwayland);
+    wmoon_build.addBuildOption([:0]const u8, "version", full_version);
+    addServerDeps(wmoon_build, scanner);
+    wmoon_build.install();
 
-        addServerDeps(river, scanner);
-
-        river.install();
+    // Run main executable
+    const wmoon_run = wmoon_build.run();
+    wmoon_run.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        wmoon_run.addArgs(args);
     }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&wmoon_run.step);
 
-    {
-        const riverctl = b.addExecutable("riverctl", "riverctl/main.zig");
-        riverctl.setTarget(target);
-        riverctl.setBuildMode(mode);
-        riverctl.addBuildOption([:0]const u8, "version", full_version);
-
-        riverctl.step.dependOn(&scanner.step);
-        riverctl.addPackage(scanner.getPkg());
-        riverctl.addPackagePath("flags", "common/flags.zig");
-        riverctl.linkLibC();
-        riverctl.linkSystemLibrary("wayland-client");
-
-        scanner.addCSource(riverctl);
-
-        riverctl.install();
-    }
-
-    {
-        const rivertile = b.addExecutable("rivertile", "rivertile/main.zig");
-        rivertile.setTarget(target);
-        rivertile.setBuildMode(mode);
-        rivertile.addBuildOption([:0]const u8, "version", full_version);
-
-        rivertile.step.dependOn(&scanner.step);
-        rivertile.addPackage(scanner.getPkg());
-        rivertile.addPackagePath("flags", "common/flags.zig");
-        rivertile.linkLibC();
-        rivertile.linkSystemLibrary("wayland-client");
-
-        scanner.addCSource(rivertile);
-
-        rivertile.install();
-    }
-
-    {
-        const file = try fs.path.join(b.allocator, &[_][]const u8{ b.cache_root, "river-protocols.pc" });
-        const pkgconfig_file = try std.fs.cwd().createFile(file, .{});
-
-        const writer = pkgconfig_file.writer();
-        try writer.print(
-            \\prefix={s}
-            \\datadir=${{prefix}}/share
-            \\pkgdatadir=${{datadir}}/river-protocols
-            \\
-            \\Name: river-protocols
-            \\URL: https://github.com/riverwm/river
-            \\Description: protocol files for the river wayland compositor
-            \\Version: {s}
-        , .{ b.install_prefix, full_version });
-        defer pkgconfig_file.close();
-
-        b.installFile("protocol/river-layout-v3.xml", "share/river-protocols/river-layout-v3.xml");
-        b.installFile(file, "share/pkgconfig/river-protocols.pc");
-    }
-
-    if (man_pages) {
-        const scdoc_step = ScdocStep.create(b);
-        try scdoc_step.install();
-    }
-
-    if (bash_completion) {
-        b.installFile("completions/bash/riverctl", "share/bash-completion/completions/riverctl");
-    }
-
-    if (zsh_completion) {
-        b.installFile("completions/zsh/_riverctl", "share/zsh/site-functions/_riverctl");
-    }
-
-    if (fish_completion) {
-        b.installFile("completions/fish/riverctl.fish", "share/fish/vendor_completions.d/riverctl.fish");
-    }
-
-    {
-        const river_test = b.addTest("river/test_main.zig");
-        river_test.setTarget(target);
-        river_test.setBuildMode(mode);
-        river_test.addBuildOption(bool, "xwayland", xwayland);
-
-        addServerDeps(river_test, scanner);
-
-        const test_step = b.step("test", "Run the tests");
-        test_step.dependOn(&river_test.step);
-    }
+    // Test step
+    const wmoon_test = b.addTest("src/test_main.zig");
+    wmoon_test.setTarget(target);
+    wmoon_test.setBuildMode(mode);
+    wmoon_test.addBuildOption(bool, "xwayland", xwayland);
+    addServerDeps(wmoon_test, scanner);
+    const test_step = b.step("test", "Run the tests");
+    test_step.dependOn(&wmoon_test.step);
 }
 
 fn addServerDeps(exe: *zbs.LibExeObjStep, scanner: *ScanProtocolsStep) void {
@@ -206,63 +109,9 @@ fn addServerDeps(exe: *zbs.LibExeObjStep, scanner: *ScanProtocolsStep) void {
     exe.addPackage(wlroots);
     exe.linkSystemLibrary("wlroots");
 
-    exe.addPackagePath("flags", "common/flags.zig");
-    exe.addCSourceFile("river/wlroots_log_wrapper.c", &[_][]const u8{ "-std=c99", "-O2" });
+    exe.addPackagePath("flags", "flags.zig");
+    exe.addCSourceFile("src/wlroots_log_wrapper.c", &[_][]const u8{ "-std=c99", "-O2" });
 
     // TODO: remove when zig issue #131 is implemented
     scanner.addCSource(exe);
 }
-
-const ScdocStep = struct {
-    const scd_paths = [_][]const u8{
-        "doc/river.1.scd",
-        "doc/riverctl.1.scd",
-        "doc/rivertile.1.scd",
-    };
-
-    builder: *zbs.Builder,
-    step: zbs.Step,
-
-    fn create(builder: *zbs.Builder) *ScdocStep {
-        const self = builder.allocator.create(ScdocStep) catch @panic("out of memory");
-        self.* = init(builder);
-        return self;
-    }
-
-    fn init(builder: *zbs.Builder) ScdocStep {
-        return ScdocStep{
-            .builder = builder,
-            .step = zbs.Step.init(.Custom, "Generate man pages", builder.allocator, make),
-        };
-    }
-
-    fn make(step: *zbs.Step) !void {
-        const self = @fieldParentPtr(ScdocStep, "step", step);
-        for (scd_paths) |path| {
-            const command = try std.fmt.allocPrint(
-                self.builder.allocator,
-                "scdoc < {s} > {s}",
-                .{ path, path[0..(path.len - 4)] },
-            );
-            _ = try self.builder.exec(&[_][]const u8{ "sh", "-c", command });
-        }
-    }
-
-    fn install(self: *ScdocStep) !void {
-        self.builder.getInstallStep().dependOn(&self.step);
-
-        for (scd_paths) |path| {
-            const path_no_ext = path[0..(path.len - 4)];
-            const basename_no_ext = fs.path.basename(path_no_ext);
-            const section = path_no_ext[(path_no_ext.len - 1)..];
-
-            const output = try std.fmt.allocPrint(
-                self.builder.allocator,
-                "share/man/man{s}/{s}",
-                .{ section, basename_no_ext },
-            );
-
-            self.builder.installFile(path_no_ext, output);
-        }
-    }
-};
